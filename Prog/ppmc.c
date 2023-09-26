43f0717b522a604d93668d488b2c706f77c4bd2f
Arian Ubuntu <ariandervishaj@icloud.com>
2023-09-26 11:58:35 +0200

    Algo : Bonne pratique

--- Prog/ppmc.c
+++ Prog/ppmc.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 #include <math.h>
+int ppmc(int nb1, int nb2);
+
+int main() {
+    int nb1 = 15, nb2 = 12;
+    printf("Le ppmc de %d et %d est %d\n",nb1,nb2,ppmc(nb1, nb2));
+    return 0;
+}
+
 int ppmc(int nb1, int nb2){
     int mult_nb1 = nb1, mult_nb2 =nb2;
 
@@ -13,10 +21,4 @@ int ppmc(int nb1, int nb2){
         }
     }
     return mult_nb1;
-}
-
-int main() {
-    int nb1 = 15, nb2 = 12;
-    printf("Le ppmc de %d et %d est %d\n",nb1,nb2,ppmc(nb1, nb2));
-    return 0;
 }
